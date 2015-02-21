da665aa3e9ace287086c9d93dceebedad4cb89b3
Rail <rail.suleymanov@gmail.com>
2015-02-21 19:03:24 +0300

    My first submission

--- higgsml_logreg.py
+++ higgsml_logreg.py
@@ -10,7 +10,7 @@ def AMS(s, b) :
 
 # Break train/test data files to make them suitable for training/testing
 # (according to NA's structure in features)
-import csv, numpy as np
+import os, csv, numpy as np
 import break_data, logreg_1_file
 
 training_names = ["training (A).csv", "training (B).csv", "training (C).csv",
@@ -31,53 +31,147 @@ for test_file in testing_names :
 # Train logistic regression for each data subset
 cols_to_remove = [0, 23, 31, 32]
 colsA = [j for j in range(33) if not j in cols_to_remove]
-print "Performing logistic regression on A dataset"
-print "\tExtracting data..."
-events_A, X_A, weights_A, labels_A, y_A = extract_data(training_names[0], colsA)
-print "\tFinished extracting data."
-ThetaA = log_regression(X_A, y_A)
-
 cols_to_remove = [0, 1, 23, 31, 32]
 colsB = [j for j in range(33) if not j in cols_to_remove]
-print "Performing logistic regression on B dataset"
-print "\tExtracting data..."
-events_B, X_B, weights_B, labels_B, y_B = extract_data(training_names[1], colsB)
-print "\tFinished extracting data."
-ThetaB = log_regression(X_B, y_B)
-
 cols_to_remove = [0, 5, 6, 7, 13, 23, 27, 28, 29, 31, 32]
 colsC = [j for j in range(33) if not j in cols_to_remove]
-print "Performing logistic regression on C dataset"
-print "\tExtracting data..."
-events_C, X_C, weights_C, labels_C, y_C = extract_data(training_names[2], colsC)
-print "\tFinished extracting data."
-ThetaC = log_regression(X_C, y_C)
-
 cols_to_remove = [0, 1, 5, 6, 7, 13, 23, 27, 28, 29, 31, 32]
 colsD = [j for j in range(33) if not j in cols_to_remove]
-print "Performing logistic regression on D dataset"
-print "\tExtracting data..."
-events_D, X_D, weights_D, labels_D, y_D = extract_data(training_names[3], colsD)
-print "\tFinished extracting data."
-ThetaD = log_regression(X_D, y_D)
-
 cols_to_remove = [0, 5, 6, 7, 13, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]
 colsE = [j for j in range(33) if not j in cols_to_remove]
-print "Performing logistic regression on E dataset"
-print "\tExtracting data..."
-events_E, X_E, weights_E, labels_E, y_E = extract_data(training_names[4], colsE)
-print "\tFinished extracting data."
-ThetaE = log_regression(X_E, y_E)
-
 cols_to_remove = [0, 1, 5, 6, 7, 13, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]
 colsF = [j for j in range(33) if not j in cols_to_remove]
-print "Performing logistic regression on F dataset"
-print "\tExtracting data..."
-events_F, X_F, weights_F, labels_F, y_F = extract_data(training_names[5], colsF)
-print "\tFinished extracting data."
-ThetaF = log_regression(X_F, y_F)
 
+skip_training = True
+
+if not skip_training :
+	print "Performing logistic regression on A dataset"
+	print "\tExtracting data..."
+	events_A, X_A, weights_A, labels_A, y_A = logreg_1_file.extract_data \
+											(training_names[0], colsA)
+	print "\tFinished extracting data."
+	ThetaA = logreg_1_file.log_regression(X_A, y_A)
+
+	print "Performing logistic regression on B dataset"
+	print "\tExtracting data..."
+	events_B, X_B, weights_B, labels_B, y_B = logreg_1_file.extract_data \
+											(training_names[1], colsB)
+	print "\tFinished extracting data."
+	ThetaB = logreg_1_file.log_regression(X_B, y_B)
+
+	print "Performing logistic regression on C dataset"
+	print "\tExtracting data..."
+	events_C, X_C, weights_C, labels_C, y_C = logreg_1_file.extract_data \
+											(training_names[2], colsC)
+	print "\tFinished extracting data."
+	ThetaC = logreg_1_file.log_regression(X_C, y_C)
+
+	print "Performing logistic regression on D dataset"
+	print "\tExtracting data..."
+	events_D, X_D, weights_D, labels_D, y_D = logreg_1_file.extract_data \
+											(training_names[3], colsD)
+	print "\tFinished extracting data."
+	ThetaD = logreg_1_file.log_regression(X_D, y_D)
+
+	print "Performing logistic regression on E dataset"
+	print "\tExtracting data..."
+	events_E, X_E, weights_E, labels_E, y_E = logreg_1_file.extract_data \
+											(training_names[4], colsE)
+	print "\tFinished extracting data."
+	ThetaE = logreg_1_file.log_regression(X_E, y_E)
+
+	print "Performing logistic regression on F dataset"
+	print "\tExtracting data..."
+	events_F, X_F, weights_F, labels_F, y_F = logreg_1_file.extract_data \
+											(training_names[5], colsF)
+	print "\tFinished extracting data."
+	ThetaF = logreg_1_file.log_regression(X_F, y_F)
+	
+	np.savetxt("ThetaA.csv", ThetaA, fmt = "%s", delimiter = ",")
+	np.savetxt("ThetaB.csv", ThetaB, fmt = "%s", delimiter = ",")
+	np.savetxt("ThetaC.csv", ThetaC, fmt = "%s", delimiter = ",")
+	np.savetxt("ThetaD.csv", ThetaD, fmt = "%s", delimiter = ",")
+	np.savetxt("ThetaE.csv", ThetaE, fmt = "%s", delimiter = ",")
+	np.savetxt("ThetaF.csv", ThetaF, fmt = "%s", delimiter = ",")
+else :
+	data = list(csv.reader(open("ThetaA.csv", "rb"), delimiter = ","))
+	ThetaA = (np.array([map(float, row) for row in data])).transpose()
+	data = list(csv.reader(open("ThetaB.csv", "rb"), delimiter = ","))
+	ThetaB = (np.array([map(float, row) for row in data])).transpose()
+	data = list(csv.reader(open("ThetaC.csv", "rb"), delimiter = ","))
+	ThetaC = (np.array([map(float, row) for row in data])).transpose()
+	data = list(csv.reader(open("ThetaD.csv", "rb"), delimiter = ","))
+	ThetaD = (np.array([map(float, row) for row in data])).transpose()
+	data = list(csv.reader(open("ThetaE.csv", "rb"), delimiter = ","))
+	ThetaE = (np.array([map(float, row) for row in data])).transpose()
+	data = list(csv.reader(open("ThetaF.csv", "rb"), delimiter = ","))
+	ThetaF = (np.array([map(float, row) for row in data])).transpose()
+
+# Compute AMS score on train data
+
+	
 # Read test data and compute predictions
+print "Computing score on A test dataset"
+print "\tExtracting data..."
+events_A, X_A_test = logreg_1_file.extract_data(testing_names[0], colsA, True)
+X_A_test = logreg_1_file.scale_features(X_A_test)
+print "\tFinished extracting data."
+A_probs = logreg_1_file.eval_probs(X_A_test, ThetaA)
 
+print "Computing score on B test dataset"
+print "\tExtracting data..."
+events_B, X_B_test = logreg_1_file.extract_data(testing_names[1], colsB, True)
+X_B_test = logreg_1_file.scale_features(X_B_test)
+print "\tFinished extracting data."
+B_probs = logreg_1_file.eval_probs(X_B_test, ThetaB)
 
-# Sort and record results to submission file
+print "Computing score on C test dataset"
+print "\tExtracting data..."
+events_C, X_C_test = logreg_1_file.extract_data(testing_names[2], colsC, True)
+X_C_test = logreg_1_file.scale_features(X_C_test)
+print "\tFinished extracting data."
+C_probs = logreg_1_file.eval_probs(X_C_test, ThetaC)
+
+print "Computing score on D test dataset"
+print "\tExtracting data..."
+events_D, X_D_test = logreg_1_file.extract_data(testing_names[3], colsD, True)
+X_D_test = logreg_1_file.scale_features(X_D_test)
+print "\tFinished extracting data."
+D_probs = logreg_1_file.eval_probs(X_D_test, ThetaD)
+
+print "Computing score on E test dataset"
+print "\tExtracting data..."
+events_E, X_E_test = logreg_1_file.extract_data(testing_names[4], colsE, True)
+X_E_test = logreg_1_file.scale_features(X_E_test)
+print "\tFinished extracting data."
+E_probs = logreg_1_file.eval_probs(X_E_test, ThetaE)
+
+print "Computing score on F test dataset"
+print "\tExtracting data..."
+events_F, X_F_test = logreg_1_file.extract_data(testing_names[5], colsF, True)
+X_F_test = logreg_1_file.scale_features(X_F_test)
+print "\tFinished extracting data."
+F_probs = logreg_1_file.eval_probs(X_F_test, ThetaF)
+
+# Collect results, sort and record to submission file
+A_data = np.concatenate((events_A, A_probs), axis = 1)
+B_data = np.concatenate((events_B, B_probs), axis = 1)
+C_data = np.concatenate((events_C, C_probs), axis = 1)
+D_data = np.concatenate((events_D, D_probs), axis = 1)
+E_data = np.concatenate((events_E, E_probs), axis = 1)
+F_data = np.concatenate((events_F, F_probs), axis = 1)
+
+all_data = np.concatenate((A_data, B_data, C_data, D_data, E_data, F_data), \
+							axis = 0)
+
+np.savetxt("all data.csv", all_data, fmt = "%s", delimiter = ",")
+inds = all_data[:, 1].argsort()
+						
+print "Creating submission file..."					
+submission = np.array([[str(int(all_data[inds[i], 0])), str(i + 1), \
+					"s" if all_data[i, 1] >= 0.5 else "b"] \
+					for i in range(all_data.shape[0])])
+					
+submission = np.append([['EventId', 'RankOrder', 'Class']],
+						submission, axis = 0)
+np.savetxt("submission.csv", submission, fmt = "%s", delimiter = ",")
--- logreg_1_file.py
+++ logreg_1_file.py
@@ -11,26 +11,28 @@ def scale_features(X) :
 		maxj = max(X[:, j])
 		X[:, j] = (X[:, j] - minj) / (maxj - minj)
 	return X
+	
+def eval_probs(X, Theta) :
+	(m, n) = X.shape
+	probs = np.array([[logreg.sigmoid(np.dot(X[i, :], Theta.transpose()))] \
+			for i in range(m)])
+	return probs
 		
 def score(X, y, Theta) :
 	(m, n) = X.shape
 	count = 0
-	probs = [logreg.sigmoid(np.dot(X[i, :], Theta.transpose())) for i in range(m)]
+	probs = eval_probs(X, Theta)
 	pred = np.array([1 if prob >= 0.5 else 0 for prob in probs], int)
 	for i in range(len(pred)) :
 		if y[i] == pred[i] :
 			count += 1
-	return 1.0 * count / (1.0 * m) * 100.0, probs
+	return 1.0 * count / (1.0 * m) * 100.0
 	
 def extract_data(filename, cols, test = False) :
 	assert os.path.exists(filename)
 	data = list(csv.reader(open(filename, "rb"), delimiter = ","))
 	X = np.array([map(float, [row[j] for j in cols]) for row in data[1:]])
-	# events = np.array([map(int, row[0] for row in data[1:])])
-	# weights = np.array([map(float, row[-2] for row in data[1:])])
-	# events = np.array([map(int, row[0]) for row in data[1:]])
-	# weights = np.array([map(float, row[-2]) for row in data[1:]])
-	events = np.array([int(row[0]) for row in data[1:]])
+	events = np.array([[int(row[0])] for row in data[1:]])
 	if not test :
 		weights = np.array([float(row[-2]) for row in data[1:]])
 		labels = [row[-1] for row in data[1:]]
@@ -56,40 +58,17 @@ def log_regression(X, y) :
 	print "Score on validation data = " + str(score_validation)
 	return Theta
 	
-# def log_regression(filename, cols) :
-	# assert os.path.exists(filename)
-	# data = list(csv.reader(open(filename, "rb"), delimiter = ","))
-	# X = np.array([map(float, [row[j] for j in cols]) for row in data[1:]])
-	# (num_ex, num_f) = X.shape
-	# labels = [row[-1] for row in data[1:]]
-	# y = np.array([[1 if label == "s" else 0 for label in labels]]).transpose()
-	# X = scale_features(X) # scale features to have them in [0, 1] range
-	# random_permute = random.sample(range(num_ex), num_ex)
-	# num_train = int(num_ex * 0.9)
-	# num_validation = num_ex - num_train
-	# X_train = X[random_permute[:num_train], :]
-	# y_train = y[random_permute[:num_train]]
-	# X_validation = X[random_permute[num_train:], :]
-	# y_validation = y[random_permute[num_train:]]
-	# Theta = logreg.logregBFGS(X_train, y_train)
-	# score_training = score(X_train, y_train, Theta)
-	# score_validation = score(X_validation, y_validation, Theta)
-	# print "Score on training data = " + str(score_training)
-	# print "Score on validation data = " + str(score_validation) + "\n"
-	# return Theta
-	
 # for testing
 if __name__ == '__main__' :
 	# break_data(sys.argv[1], sys.argv[2])
 	
 	# A set contains only defined values
-	training_f = "training (A).csv"
-	cols_to_remove = [0, 23, 31, 32]
-	cols = [j for j in range(33) if not j in cols_to_remove]
-	print "Performing logistic regression on A dataset"
-	events, X, weights, labels, y = extract_data(training_f, cols)
-	# Theta = log_regression(training_f, cols)
-	Theta = log_regression(X, y)
+	# training_f = "training (A).csv"
+	# cols_to_remove = [0, 23, 31, 32]
+	# cols = [j for j in range(33) if not j in cols_to_remove]
+	# print "Performing logistic regression on A dataset"
+	# events, X, weights, labels, y = extract_data(training_f, cols)
+	# Theta = log_regression(X, y)
 	
 	# B set shouldn't contain "DER_mass_MMC" (1) column
 	training_f = "training (B).csv"
@@ -97,49 +76,44 @@ if __name__ == '__main__' :
 	cols = [j for j in range(33) if not j in cols_to_remove]
 	print "Performing logistic regression on B dataset"
 	events, X, weights, labels, y = extract_data(training_f, cols)
-	# Theta = log_regression(training_f, cols)
 	Theta = log_regression(X, y)
 	
 	# C set shouldn't contain "DER_deltaeta_jet_jet" (5) column
 	# AND, as well, "DER_mass_jet_jet" (6), "DER_prodeta_jet_jet" (7), 
 	# "DER_lep_eta_centrality" (13), "PRI_jet_subleading_pt" (27),
 	# "PRI_jet_subleading_eta" (28) and "PRI_jet_subleading_phi" (29) columns
-	training_f = "training (C).csv"
-	cols_to_remove = [0, 5, 6, 7, 13, 23, 27, 28, 29, 31, 32]
-	cols = [j for j in range(33) if not j in cols_to_remove]
-	print "Performing logistic regression on C dataset"
-	events, X, weights, labels, y = extract_data(training_f, cols)
-	# Theta = log_regression(training_f, cols)
-	Theta = log_regression(X, y)
+	# training_f = "training (C).csv"
+	# cols_to_remove = [0, 5, 6, 7, 13, 23, 27, 28, 29, 31, 32]
+	# cols = [j for j in range(33) if not j in cols_to_remove]
+	# print "Performing logistic regression on C dataset"
+	# events, X, weights, labels, y = extract_data(training_f, cols)
+	# Theta = log_regression(X, y)
 	
 	# D set shouldn't contain "DER_mass_MMC" and "DER_deltaeta_jet_jet" columns
-	training_f = "training (D).csv"
-	cols_to_remove = [0, 1, 5, 6, 7, 13, 23, 27, 28, 29, 31, 32]
-	cols = [j for j in range(33) if not j in cols_to_remove]
-	print "Performing logistic regression on D dataset"
-	events, X, weights, labels, y = extract_data(training_f, cols)
-	# Theta = log_regression(training_f, cols)
-	Theta = log_regression(X, y)
+	# training_f = "training (D).csv"
+	# cols_to_remove = [0, 1, 5, 6, 7, 13, 23, 27, 28, 29, 31, 32]
+	# cols = [j for j in range(33) if not j in cols_to_remove]
+	# print "Performing logistic regression on D dataset"
+	# events, X, weights, labels, y = extract_data(training_f, cols)
+	# Theta = log_regression(X, y)
 	
 	# E set shouldn't contain "DER_deltaeta_jet_jet" and 
 	# "PRI_jet_leading_pt" (24) columns, AND, as well, 
 	# "PRI_jet_leading_eta" (25) and "PRI_jet_leading_phi" (26) columns
 	# (and "PRI_jet_all_pt", because for this case it's all gonna be 0)
-	training_f = "training (E).csv"
-	cols_to_remove = [0, 5, 6, 7, 13, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]
-	cols = [j for j in range(33) if not j in cols_to_remove]
-	print "Performing logistic regression on E dataset"
-	events, X, weights, labels, y = extract_data(training_f, cols)
-	# Theta = log_regression(training_f, cols)
-	Theta = log_regression(X, y)
+	# training_f = "training (E).csv"
+	# cols_to_remove = [0, 5, 6, 7, 13, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]
+	# cols = [j for j in range(33) if not j in cols_to_remove]
+	# print "Performing logistic regression on E dataset"
+	# events, X, weights, labels, y = extract_data(training_f, cols)
+	# Theta = log_regression(X, y)
 	
 	# F set shouldn't contain "DER_mass_MMC", "DER_deltaeta_jet_jet" and
 	# "PRI_jet_leading_pt" columns
 	# (and "PRI_jet_all_pt", because for this case it's all gonna be 0)
-	training_f = "training (F).csv"
-	cols_to_remove = [0, 1, 5, 6, 7, 13, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]
-	cols = [j for j in range(33) if not j in cols_to_remove]
-	print "Performing logistic regression on F dataset"
-	events, X, weights, labels, y = extract_data(training_f, cols)
-	# Theta = log_regression(training_f, cols)
-	Theta = log_regression(X, y)
+	# training_f = "training (F).csv"
+	# cols_to_remove = [0, 1, 5, 6, 7, 13, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]
+	# cols = [j for j in range(33) if not j in cols_to_remove]
+	# print "Performing logistic regression on F dataset"
+	# events, X, weights, labels, y = extract_data(training_f, cols)
+	# Theta = log_regression(X, y)
